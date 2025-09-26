20f956a0de3dd333ae9889b994c25a11587a717d
Cristhian Alvarez <64036218+Crissancio@users.noreply.github.com>
2025-09-26 18:30:07 +0000

    clases

--- clase2609/Comida.java
+++ clase2609/Comida.java
@@ -1,5 +1,10 @@
 package clase2609;
 
-public class Comida {
-    
+public class Comida extends Producto{
+    private int calorias;
+
+    public Comida(String nombre, double precio, int calorias){
+        super(nombre, precio);
+        this.calorias = calorias;
+    }
 }
---
+++ clase2609/Toalla.java
@@ -0,0 +1,12 @@
+package clase2609;
+
+public class Toalla extends Producto{
+    private float largo, ancho;
+
+    public Toalla(String nombre, double precio, float largo, float ancho){
+        super(nombre, precio);
+        this.largo = largo;
+        this.ancho = ancho;
+
+    }
+}
