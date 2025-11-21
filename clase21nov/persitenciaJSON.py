e879852ef46dbd8b8aeb809f81265bf536c7b1de
Cristhian Alvarez <64036218+Crissancio@users.noreply.github.com>
2025-11-21 18:47:22 +0000

    persistencia 2

--- clase21nov/persitenciaJSON.py
+++ clase21nov/persitenciaJSON.py
@@ -2,8 +2,72 @@ import json
 
 persona = {"nombre": "Ana", "edad": 20}
 
+class Persona:
+    def __init__(self, nom = "", edad = 0, peso = 0):
+        self.nombre = nom
+        self.edad = edad
+        self.peso = peso
+    
+    def mostrar(self):
+        print(self.nombre, self.edad, self.peso)
+    
+    def guardarArchivoJSON(self):
+        datos = {
+            "nombre": self.nombre,
+            "peso": self.peso,
+            "edad": self.edad
+        }
+        try:
+            with open("/workspaces/Programacion-II-ParE-II-25/clase21nov/persona.json", "w") as file:
+                json.dump(datos, file)
+        except IOError as e:
+            print(e)
+    def crearDesdeArchivo(self, direccionArchivo):
+        try:
+            with open(direccionArchivo, "r") as a:
+                datos = json.load(a)
+                self.nombre = datos["nombre"]
+                self.edad = datos["edad"]
+                self.peso = datos["peso"]
+        except FileNotFoundError:
+            print("El archivo no existe")
+        except json.JSONDecodeError:
+            print("El archivo no contiene JSON válido")
+
+class Mascota:
+    def __init__(self, nombre = "", especie= "", edad= 0):
+        self.nombre = nombre
+        self.especie = especie
+        self.edad = edad
+    def mostrar(self):
+        print("MASCOTA:", self.nombre, self.edad, self.especie)
+    
+    def guardarArchivo(self):
+        datos = {
+            "nombre": self.nombre,
+            "edad": self.edad,
+            "esepcie": self.especie
+        }
+        try:
+            with open("mascota.json", "w") as f:
+                json.dump(datos, f)
+        except IOError as e:
+            print("Error al escribir el archivo:", e)
+    
+    def cargarArchivo(self, ruta):
+        datos = {}
+        try:
+            with open("persona.json", "r") as f:
+                persona = json.load(f)
+            print("Nombre:", persona["nombre"])
+        except FileNotFoundError:
+            print("El archivo no existe")
+        except json.JSONDecodeError:
+            print("El archivo no contiene JSON válido")
+        
 
 # GUARDAR
+'''
 try:
     with open("persona.json", "w") as f:
         json.dump(persona, f)
@@ -20,3 +84,13 @@ except FileNotFoundError:
     print("El archivo no existe")
 except json.JSONDecodeError:
     print("El archivo no contiene JSON válido")
+'''
+p = Persona("Jose", 20, 70)
+p.mostrar()
+p.guardarArchivoJSON()
+
+ruta = "/workspaces/Programacion-II-ParE-II-25/persona.json"
+
+p1 = Persona()
+p1.crearDesdeArchivo(ruta)
+p1.mostrar()
